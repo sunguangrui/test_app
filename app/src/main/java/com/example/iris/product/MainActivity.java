a5e89e190d9cbf9b1a0a52dc175ea51d4643b1e5
sunguangrui <277061708@qq.com>
2018-04-10 16:11:31 +0800

    add b.txt

--- app/src/main/java/com/example/iris/product/MainActivity.java
+++ app/src/main/java/com/example/iris/product/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends BaseActivity implements
         int date1=20180411;
         clearWebViewCache();//清除掉缓存
         surl = CadillacUrl.HOME_URL;
+        zan cun----------
     }
 
     @Override
