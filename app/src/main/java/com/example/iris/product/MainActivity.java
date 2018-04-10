bba45497f21d058a95fc19d44e18350c2ac5e0c8
sunguangrui <277061708@qq.com>
2018-04-10 17:35:18 +0800

    sdafs

--- app/src/main/java/com/example/iris/product/MainActivity.java
+++ app/src/main/java/com/example/iris/product/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends BaseActivity implements
         surl = CadillacUrl.HOME_URL;
 99999999999999999999999999999999999
         1111111111111111
-
+       sgr code   
     }
 
     @Override
